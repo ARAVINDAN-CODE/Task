b714d9b44e784dc6f98d632634d6e82da33dd4c6
ARAVINDAN-CODE <139460310+ARAVINDAN-CODE@users.noreply.github.com>
2023-08-25 17:20:10 +0530

    src files

---
+++ TASK-10/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Nav from "./Components/Nav";
+import 'bootstrap/dist/css/bootstrap.css';
+import Home from "./Components/Home"
+
+function App() {
+  return (
+    <div>
+      <Nav />
+    
+      
+    </div>
+  );
+}
+
+export default App;
